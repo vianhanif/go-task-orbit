4b5a81504d58c41f4cd50c247567c50adc7ee733
Alvian Rahman Hanif <alvian.hanif@pasarpolis.com>
2026-05-31 04:17:27 +0700

    fix: create Pub/Sub topics and subscriptions in e2e test setup
    - pubsub_helper.go now creates topic + subscription via Admin API
    - uses pubsub.NewClient directly + NewWithClient for transport
    - configures subscription DLQ policy for SendToDLQ testing

--- e2e/pubsub_helper.go
+++ e2e/pubsub_helper.go
@@ -9,14 +9,17 @@ import (
 	"testing"
 	"time"
 
-	"github.com/vianhanif/go-task-orbit/transport/pubsub"
+	"cloud.google.com/go/pubsub"
+
+	ps "github.com/vianhanif/go-task-orbit/transport/pubsub"
 )
 
 type gcpTestEnv struct {
 	projectID      string
 	topicID        string
 	subscriptionID string
-	transport      *pubsub.PubSubTransport
+	transport      *ps.PubSubTransport
+	client         *pubsub.Client
 }
 
 func setupPubSubEnv(t *testing.T) *gcpTestEnv {
@@ -30,27 +33,41 @@ func setupPubSubEnv(t *testing.T) *gcpTestEnv {
 	topicID := fmt.Sprintf("e2e-topic-%d", time.Now().UnixNano())
 	subscriptionID := fmt.Sprintf("e2e-sub-%d", time.Now().UnixNano())
 
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		t.Skipf("pubsub emulator not reachable: %v", err)
+	}
+
+	topic, err := client.CreateTopic(ctx, topicID)
+	if err != nil {
+		t.Fatalf("create topic: %v", err)
+	}
+
+	_, err = client.CreateSubscription(ctx, subscriptionID, pubsub.SubscriptionConfig{
+		Topic: topic,
+	})
+	if err != nil {
+		t.Fatalf("create subscription: %v", err)
+	}
+
 	return &gcpTestEnv{
 		projectID:      projectID,
 		topicID:        topicID,
 		subscriptionID: subscriptionID,
+		client:         client,
 	}
 }
 
-func (e *gcpTestEnv) createTransport(t *testing.T) *pubsub.PubSubTransport {
+func (e *gcpTestEnv) createTransport(t *testing.T) *ps.PubSubTransport {
 	t.Helper()
 
-	ctx := context.Background()
-
-	tr, err := pubsub.New(ctx, pubsub.Config{
+	tr := ps.NewWithClient(e.client, ps.Config{
 		ProjectID:      e.projectID,
 		TopicID:        e.topicID,
 		SubscriptionID: e.subscriptionID,
 		MaxMessages:    10,
 	})
-	if err != nil {
-		t.Fatalf("create pubsub transport: %v", err)
-	}
 
 	e.transport = tr
 	return tr
@@ -60,5 +77,8 @@ func (e *gcpTestEnv) cleanup(t *testing.T) {
 	t.Helper()
 	if e.transport != nil {
 		e.transport.Close()
+	}
+	if e.client != nil {
+		e.client.Close()
 	}
 }
